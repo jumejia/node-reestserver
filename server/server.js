3eab52c0529a8a2b4ec157e932297c975c9a79a0
Julian Mejia <julian78993@gmail.com>
2019-09-18 11:39:22 -0500

    usario sección 10 test method31

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use(bodyParser.json());
 //rutas
 app.use(require('./routes/index'));
 
-mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err, res) => {
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false, useMongoClient: true }, (err, res) => {
     if (err) throw err;
 
-    console.log('Db conected to localhost:27017/cafe');
+    console.log('Db conected ');
 });
 
 app.listen(process.env.PORT, () => {
